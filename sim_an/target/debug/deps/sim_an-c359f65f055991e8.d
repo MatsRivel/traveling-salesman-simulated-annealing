7ed16d0563e24755875712aa5c6f4c84b922a91c
MatsRivel <mats-r@hotmail.com>
2023-07-11 10:50:23 +0200

    Initial branch commit

--- sim_an/target/debug/deps/sim_an-c359f65f055991e8.d
+++ sim_an/target/debug/deps/sim_an-c359f65f055991e8.d
@@ -1,6 +1,6 @@
-c:\Users\rivelandm\OneDrive - NOV Inc\Documents\Other\traveling-salesman-simulated-annealing\sim_an\target\debug\deps\sim_an-c359f65f055991e8.rmeta: src\main.rs src\constructors.rs src\constants.rs src\validity_check.rs src\correctors.rs src\alter_solution.rs
+C:\Users\rivelandm\OneDrive - NOV Inc\Documents\Other\traveling-salesman-simulated-annealing\sim_an\target\debug\deps\sim_an-c359f65f055991e8.rmeta: src\main.rs src\constructors.rs src\constants.rs src\validity_check.rs src\correctors.rs src\alter_solution.rs
 
-c:\Users\rivelandm\OneDrive - NOV Inc\Documents\Other\traveling-salesman-simulated-annealing\sim_an\target\debug\deps\sim_an-c359f65f055991e8.d: src\main.rs src\constructors.rs src\constants.rs src\validity_check.rs src\correctors.rs src\alter_solution.rs
+C:\Users\rivelandm\OneDrive - NOV Inc\Documents\Other\traveling-salesman-simulated-annealing\sim_an\target\debug\deps\sim_an-c359f65f055991e8.d: src\main.rs src\constructors.rs src\constants.rs src\validity_check.rs src\correctors.rs src\alter_solution.rs
 
 src\main.rs:
 src\constructors.rs:
